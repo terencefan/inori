a8c10afb1df1e5a831be7140d634a2d5824baeb0
stdrickforce <stdrickforce@gmail.com>
2013-09-26 18:02:37 +0800

    index html

--- inori/flaskr.py
+++ inori/flaskr.py
@@ -1,6 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0')
