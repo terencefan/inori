fe536f3a00d14c6f18c23012dda43ac8ac6bee44
stdrickforce <stdrickforce@gmail.com>
2014-09-03 21:54:21 +0800

    models

--- inori/models/__init__.py
+++ inori/models/__init__.py
@@ -1,3 +1,26 @@
-from .database import dbsession
-from .account import *
-from .home import *
+# -*- coding:utf-8 -*-
+
+from inori.settings import mysql_settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+master_url = ("mysql://{user}:{passwd}@{host}:{port}/{database}"
+              "?charset=utf8".format(**mysql_settings))
+engine = create_engine(
+    master_url,
+    convert_unicode=True,
+    pool_size=10,
+    max_overflow=-1,
+    pool_recycle=3600
+)
+
+DBSession = scoped_session(
+    sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+Base = declarative_base()
+Base.query = DBSession.query_property()
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
