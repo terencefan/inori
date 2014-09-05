9296447823ed3a14f17accb53adca6263879a5e4
stdrickforce <stdrickforce@gmail.com>
2014-09-06 07:07:58 +0800

    add sound

--- inori/static/js/inori.js
+++ inori/static/js/inori.js
@@ -1,7 +1,21 @@
 function showInfo(message) {
-  alert('info');
+  $._messengerDefaults = {
+    extraClasses: 'messenger-fixed messenger-theme-future messenger-on-bottom'
+  }
+  $.globalMessenger().post({
+    message: message,
+    type: 'info',
+    showCloseButton: true,
+  });
 }
 
 function showError(message) {
-  alert('error');
+  $._messengerDefaults = {
+    extraClasses: 'messenger-fixed messenger-theme-future messenger-on-top'
+  }
+  $.globalMessenger().post({
+    message: message,
+    type: 'error',
+    showCloseButton: true,
+  });
 }
